671a2f2d3af91484793ac9257c590fbd7e90eadf
gary16 <jialiang1988@live.cn>
2018-07-24 15:24:38 +0800

    修改逻辑

--- gwork_app/src/main/java/com/gwork/app/springdemo/distributionlock/DistributionLock.java
+++ gwork_app/src/main/java/com/gwork/app/springdemo/distributionlock/DistributionLock.java
@@ -1,26 +1,57 @@
 package com.gwork.app.springdemo.distributionlock;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 public class DistributionLock {
 
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DistributionLock.class);
+	
 	@Autowired
 	private JedisPool jedisPool;// 注入JedisPool
 	
 	private int waitMs;
 
-	public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+	public boolean tryLock(String key,int waitSecond,int lockSecond) throws InterruptedException {
+		Jedis jedis = jedisPool.getResource();
+		boolean flg=false;
+		 try {
+			 if(waitSecond<=0) {
+				 flg = jedis.setnx(key, "1")==1 ;
+			 }else {
+				 for(long i=0;i<waitSecond;i++) {
+					 flg = jedis.setnx(key, "1")==1 ;
+					 if(flg) {
+						 break;
+					 }
+					 Thread.sleep(1000);
+				 }
+			 }
+			 if(flg) {
+				 jedis.expire(key, seconds) 
+			 }
+		 }
+		 catch(Exception e){
+			 LOGGER.error(e.getMessage());
+		 }
+		return flg;
 		
-		return false;
+	}
+	
+	public boolean tryLock(String key,long waitTime) throws InterruptedException {
+		return this.tryLock(key, waitTime, 6000);
+	}
+	
+	public boolean tryLock(String key) throws InterruptedException {
+		return this.tryLock(key, 0, 6000);
 	}
 
-	public void unlock() {
+	public void unlock(String key) {
 
 	}
 
